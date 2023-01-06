92170c0a1d0e3155ce26bfee41e995adb35ac64e
ChrisLovric <klovric991@gmail.com>
2023-01-06 18:12:19 +0100

    izmjena u nazivu jednog proizvoda

--- www/foundation-hardshop.hr/kucista.php
+++ www/foundation-hardshop.hr/kucista.php
@@ -12,7 +12,7 @@
           
           echo '<p style="color: red;">NZXT H1 V2 matt crno, 750W 80+ Gold, 140 mm vodeno hlađenje, mini ITX</p>';
 
-          echo '<p style="color: red;">FRACTAL DESIGN Define 7 Dark TG, tempered glass, 3x 140 mm fan, USB-C, bez napajanja</p>';
+          echo '<p style="color: red;">FRACTAL DESIGN Define 7 Dark TG, tempered glass, 3x 140 mm fan, USB-C</p>';
 
         ?>
     </div>
