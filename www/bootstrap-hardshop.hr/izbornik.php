4801a8be2674dbdd40d326cca3154b96b23a5621
ChrisLovric <klovric991@gmail.com>
2023-01-06 18:10:05 +0100

    dodavanje naziva proizvoda po kategorijama

--- www/bootstrap-hardshop.hr/izbornik.php
+++ www/bootstrap-hardshop.hr/izbornik.php
@@ -1,15 +1,15 @@
 <div class="accordion" id="accordionExample">
             <div class="card bg-success">
               <h2 class="accordion-header" id="headingOne">
-                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
                   Grafičke kartice
                 </button>
               </h2>
-              <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+              <div id="collapseOne" class="accordion-collapse collapse" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>ASUS GeForce TUF-RTX4080, 16 GB GDDR6X.</strong><br>
-                  <strong>ASUS GeForce DUAL-RTX3070, 8 GB GDDR6.</strong><br>
-                  <strong>GIGABYTE AMD Radeon RX 6650 XT EAGLE, 8GB GDDR6.</strong>
+                  <strong>ASUS GeForce TUF-RTX4080, 16 GB GDDR6X</strong><br>
+                  <strong>ASUS GeForce DUAL-RTX3070, 8 GB GDDR6</strong><br>
+                  <strong>GIGABYTE AMD Radeon RX 6650 XT EAGLE, 8GB GDDR6</strong>
                 </div>
               </div>
             </div>
@@ -21,7 +21,8 @@
               </h2>
               <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the second item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <strong>AMD Ryzen 9 7950X, 4500/5700 MHz, Socket AM5, Radeon Graphics</strong><br>
+                  <strong>INTEL Core i9 12900K, 3200/5200 MHz, Socket 1700</strong><br>
                 </div>
               </div>
             </div>
@@ -33,7 +34,8 @@
               </h2>
               <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <strong>ASROCK X670E PG Lightning, socket AM5, DDR5, HDMI, DP, ATX</strong><br>
+                  <strong>ASUS ROG STRIX Z690-F Gaming WIFI, socket 1700, DDR5, HDMI, DP, Wi-Fi 6E, BT 5.2, ATX</strong>
                 </div>
               </div>
             </div>
@@ -45,7 +47,8 @@
               </h2>
               <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <strong>G.SKILL 32 GB (2x16 GB) DDR5, 6000 MHz, DIMM, Trident Z5 RGB, CL40, F5-6000U4040E16GX2-TZ5RK</strong><br>
+                  <strong>CORSAIR 64 GB (2x32 GB) DDR4, 3000 MHz, DIMM, Vengeance LPX, CL16</strong>
                 </div>
               </div>
             </div>
@@ -57,7 +60,8 @@
               </h2>
               <div id="collapseFive" class="accordion-collapse collapse" aria-labelledby="headingFive" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <strong>SSD 2 TB, SAMSUNG 990 PRO, M.2 2280, PCIe 4.0 x4 NVMe 2.0, 3-bit MLC V-NAND, MZ-V9P2T0BW</strong><br>
+                  <strong>SSD 2 TB, CORSAIR MP600, M.2 2280, PCIe 4.0 x4 NVMe, 3D TLC NAND, aluminium heatsink, CSSD-F2000GBMP600</strong>
                 </div>
               </div>
             </div>
@@ -69,7 +73,8 @@
               </h2>
               <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <strong>WESTERN DIGITAL 14 TB Red Pro, 3.5", SATA III, 7200 rpm, 512 MB, WD141KFGX</strong><br>
+                  <strong>TOSHIBA X300 14 TB, 3.5", SATA III, 7200 rpm, 256 MB, HDWR21EEZSTA</strong>
                 </div>
               </div>
             </div>
@@ -81,7 +86,8 @@
               </h2>
               <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                  <strong>SILVERSTONE SST-ST1200-PTS, 1200 W, modularno, 120 mm, 80+ Platinum</strong><br>
+                  <strong>FRACTAL DESIGN Ion+ 2 Platinum 860W, modularno, 80+ Platinum</strong>
                 </div>
               </div>
             </div>
@@ -93,7 +99,8 @@
               </h2>
               <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                <strong>NOCTUA NH-D15S Chromax Black</strong><br>
+                  <strong>BE QUIET! Dark Rock Pro 4, BK022</strong>
                 </div>
               </div>
             </div>
@@ -105,7 +112,8 @@
               </h2>
               <div id="collapseNine" class="accordion-collapse collapse" aria-labelledby="headingNine" data-bs-parent="#accordionExample">
                 <div class="accordion-body">
-                  <strong>This is the third item's accordion body.</strong> It is hidden by default, until the collapse plugin adds the appropriate classes that we use to style each element. These classes control the overall appearance, as well as the showing and hiding via CSS transitions. You can modify any of this with custom CSS or overriding our default variables. It's also worth noting that just about any HTML can go within the <code>.accordion-body</code>, though the transition does limit overflow.
+                <strong>NZXT H1 V2 matt crno, 750W 80+ Gold, 140 mm vodeno hlađenje, mini ITX</strong><br>
+                  <strong>FRACTAL DESIGN Define 7 Dark TG, tempered glass, 3x 140 mm fan, USB-C</strong>
                 </div>
               </div>
             </div>
