468b0d4d1bd0f7ba28e123e0320cf9baed7c47df
ChrisLovric <klovric991@gmail.com>
2023-01-09 15:12:15 +0100

    dodana pozadinska boja za stranice prijave i registracije

--- www/custom-hardshop.hr/registracija2.php
+++ www/custom-hardshop.hr/registracija2.php
@@ -7,6 +7,12 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Registracija</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+        <style>
+        html,
+        body {
+            background-color: ivory;
+        }
+        </style>
     </head>
 
     <body>
